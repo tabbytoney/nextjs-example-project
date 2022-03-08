c1af7e2f286a0513fa8f3c93eb50d9053d3a0744
Tabby Douglass <tabbytoney@gmail.com>
2022-03-08 16:53:29 -0600

    Getting url value/dynamic parameters

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,13 @@
 // For ./news/something-important
+import { useRouter } from 'next/router';
 
 function DetailPage() {
+  const router = useRouter();
+  // this holds the value in the url - the 'something-else' page of www.ourpage.com/something-else
+  router.query.newsId;
+
+  // could send request here to backend api to fetch news item with newsId (there isn't a db connected)
+
   return <h1>The Detail Page</h1>;
 }
 
